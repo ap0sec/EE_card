a4b43210a281771c5f23b8e145408a187be55806
junnnosuke <junji12289499@gmail.com>
2018-02-12 01:03:44 +0900

    choice_cardを作成
    ・変更： playerスクリプト内の choice_card関数＋変数追加
    手元のカードを所得して卓上のカードに情報を反映する処理。
    関数呼び出しは StartCoroutine("Choice_card")でお願いします

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -2,59 +2,89 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour {
+public class Player : MonoBehaviour
+{
 
-    public GameObject[] hand = new GameObject[5];
-    public int coin = 30;
-    public int bet = 0;
+	public GameObject[] hand = new GameObject[5];
+	public int coin = 30;
+	public int bet = 0;
 
-    //引数は自由に追加してもらって構いません
-    //その場合はsampleに従う形で何が必要なのか書いてください
-    //戻り値は基本的にvoidで mainから叩くときに何か欲しくなったらこっちからいじるかも
-    //marge・conflict消化は小高がやります
-
-
-    /* 変数宣言が必要な場合は適宜ここから*/
+	//引数は自由に追加してもらって構いません
+	//その場合はsampleに従う形で何が必要なのか書いてください
+	//戻り値は基本的にvoidで mainから叩くときに何か欲しくなったらこっちからいじるかも
+	//marge・conflict消化は小高がやります
 
 
+	/* 変数宣言が必要な場合は適宜ここから*/
+
+	public float distance = 100f;
+	public int typeDate = 0;
+	public GameObject targetCard;
+	GameObject CardObject;
+	Renderer rend;
+	Card script;
+	int looptriger = 0;
 
 
-    /*ここまで*/
+	/*ここまで*/
 
-    //sample
-    void sample(int a, double b, bool c,)
-    {
-        //a:〇〇の状態を示す変数
-        //b:✕✕の座標
-        //c:内部分岐用のフラグ
+	//sample
+	void Sample (int a, double b, bool c)
+	{
+		//a:〇〇の状態を示す変数
+		//b:✕✕の座標
+		//c:内部分岐用のフラグ
 
-        //本文
+		//本文
 
-    }
+	}
 
-    // Use this for initialization 一応残す
-	void Start () {
+	// Use this for initialization 一応残す
+	void Start ()
+	{
 		
 	}
 	
 	// Update is called once per frame 一応残す
-	void Update () {
+	void Update ()
+	{
 		
 	}
 
-    void Set_hand(int[] type) //デッキからカードを引く時用メゾット 大浦
-    {
+	void Set_hand (int[] type) //デッキからカードを引く時用メゾット 大浦
+	{
 
-    }
+	}
 
-    void Set_Bet() //ベット用メゾット 宇津木
-    {
+	void Set_Bet () //ベット用メゾット 宇津木
+	{
 
-    }
+	}
 
-    void Choice_card() //カード選択用メゾット 岩田
-    {
+	IEnumerator Choice_card () //カード選択用メゾット 岩田
+	{
+		looptriger = 0;
+		while (looptriger == 0) {
+			if (Input.GetMouseButtonDown (0)) {
+				//クリックによるGameObject所得
+				Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+				RaycastHit hit = new RaycastHit ();
 
-    }
-
+				if (Physics.Raycast (ray, out hit, distance)) {
+					//所得したCardからType変数を所得
+					CardObject = hit.collider.gameObject;
+					script = CardObject.GetComponent<Card> ();
+					typeDate = script.type;
+					//盤上のカードにデータコピー
+					targetCard.GetComponent<Card> ().type = typeDate;
+					//選択した手札を非表示化
+					rend = CardObject.GetComponent<Renderer> ();
+					rend.enabled = false;
+					looptriger = 1;
+				}
+			}
+			yield return null;
+		}
+		yield break;
+	}
 }
