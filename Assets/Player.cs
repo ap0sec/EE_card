e6b725c7427331101b8dc72e7329c8020948a1de
ap0sec <apo.t.laboratory@gmail.com>
2018-02-16 02:57:59 +0900

    [Adjust] player script adjusted
    main実装に向けて最適化。

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -6,15 +6,8 @@ using System;
 
 public class Player : MonoBehaviour
 {
-
-    public GameObject[] hand = new GameObject[5];
     public int coin = 30;
     public int bet = 0;
-
-    //引数は自由に追加してもらって構いません
-    //その場合はsampleに従う形で何が必要なのか書いてください
-    //戻り値は基本的にvoidで mainから叩くときに何か欲しくなったらこっちからいじるかも
-    //marge・conflict消化は小高がやります
 
     /* 変数宣言が必要な場合は適宜ここから*/
     GameObject targetCard;
@@ -27,23 +20,17 @@ public class Player : MonoBehaviour
 
     /*ここまで*/
 
-    //sample
-    void Sample(int a, double b, bool c)
-    {
-        //a:〇〇の状態を示す変数
-        //b:✕✕の座標
-        //c:内部分岐用のフラグ
-        //本文
-    }
 
     // Use this for initialization 一応残す
     void Start()
     {
+        coin = 30;
+        bet = 0;
         p_obj = GameObject.Find("Player");
         //Set_Betはコルーチンで呼び出す
-        StartCoroutine(p_obj.GetComponent<Player>().Set_Bet());
+        //StartCoroutine(p_obj.GetComponent<Player>().Set_Bet());
         targetCard = GameObject.FindGameObjectWithTag("target");
-        StartCoroutine("Choice_card");
+        //StartCoroutine("Choice_card");
     }
 
     // Update is called once per frame 一応残す
@@ -52,7 +39,7 @@ public class Player : MonoBehaviour
 		
     }
 
-    void Set_hand(int[] type) //デッキからカードを引く時用メゾット 大浦
+    void Set_hand(int[] type) //デッキからカードを引く時用メゾット
     {
 		int i = 0;
 
@@ -69,10 +56,8 @@ public class Player : MonoBehaviour
 		}
     }
 
-    IEnumerator Set_Bet() //ベット用メゾット 宇津木
+    IEnumerator Set_Bet() //ベット用メゾット
     {
-        //便宜的に, update()から呼び出し.
-
         GameObject textbox;
         Text ger;
 
@@ -93,7 +78,7 @@ public class Player : MonoBehaviour
 
     }
 
-    IEnumerator Choice_card() //カード選択用メゾット 岩田
+    IEnumerator Choice_card() //カード選択用メゾット
     {
         looptriger = 0;
         while (looptriger == 0)
@@ -112,7 +97,6 @@ public class Player : MonoBehaviour
 						typeDate = script.type;
 						//盤上のカードにデータコピー
 						//Debug.Log(targetCard);
-						//targetCard.GetComponent<Card> ().type = typeDate;
 						Card targetScript = targetCard.GetComponent<Card> ();
 						targetScript.type = typeDate;
 						//選択した手札を非表示化
